750b6ee1791942b767721f11eb1e79337c07ec0d
Barasa <timbaros87@gmail.com>
2024-06-17 19:44:09 +0300

    Add Direcotrs and Actors

--- src/pages/Actors.js
+++ src/pages/Actors.js
@@ -1,16 +1,62 @@
-import { useEffect, useState } from "react";
+import React from 'react';
+import NavBar from '../components/NavBar';
+
+const actors = [
+  {
+    name: 'Leonardo DiCaprio',
+    movies: ['Inception', 'The Revenant', 'Titanic', 'The Wolf of Wall Street', 'Shutter Island'],
+  },
+  {
+    name: 'Robert De Niro',
+    movies: ['Taxi Driver', 'Raging Bull', 'Goodfellas', 'The Godfather Part II', 'The Irishman'],
+  },
+  {
+    name: 'Tom Hanks',
+    movies: ['Saving Private Ryan', 'Forrest Gump', 'Cast Away', 'The Green Mile', 'Apollo 13'],
+  },
+  {
+    name: 'Morgan Freeman',
+    movies: ['The Shawshank Redemption', 'Se7en', 'Bruce Almighty', 'Driving Miss Daisy', 'Million Dollar Baby'],
+  },
+  {
+    name: 'Cate Blanchett',
+    movies: ['The Lord of the Rings: The Fellowship of the Ring', 'The Aviator', 'Carol', 'Blue Jasmine', 'Thor: Ragnarok'],
+  },
+  {
+    name: 'Samuel L. Jackson',
+    movies: ['Pulp Fiction', 'Jurassic Park', 'The Avengers', 'Django Unchained', 'Unbreakable'],
+  },
+  {
+    name: 'Johnny Depp',
+    movies: ['Pirates of the Caribbean: The Curse of the Black Pearl', 'Edward Scissorhands', 'Alice in Wonderland', 'Charlie and the Chocolate Factory', 'Sweeney Todd'],
+  },
+  {
+    name: 'Scarlett Johansson',
+    movies: ['Lost in Translation', 'The Avengers', 'Lucy', 'Marriage Story', 'Jojo Rabbit'],
+  },
+  {
+    name: 'Brad Pitt',
+    movies: ['Fight Club', 'Inglourious Basterds', 'The Curious Case of Benjamin Button', 'Seven', 'Once Upon a Time in Hollywood'],
+  },
+];
 
 function Actors() {
   return (
-    <>
-      <header>
-        {/* What component should go here? */}
-      </header>
-      <main>
-        {/* Actor info here! */}
-      </main>
-    </>
+    <div>
+      <NavBar />
+      <h1>Actors Page</h1>
+      {actors.map((actor, index) => (
+        <article key={index}>
+          <h2>{actor.name}</h2>
+          <ul>
+            {actor.movies.map((movie, idx) => (
+              <li key={idx}>{movie}</li>
+            ))}
+          </ul>
+        </article>
+      ))}
+    </div>
   );
-};
+}
 
 export default Actors;
--- src/pages/Directors.js
+++ src/pages/Directors.js
@@ -1,16 +1,67 @@
-import { useEffect, useState } from "react";
+import React from 'react';
+import NavBar from '../components/NavBar';
+
+const directors = [
+  {
+    name: 'Steven Spielberg',
+    movies: ['Jaws', 'E.T. the Extra-Terrestrial', 'Jurassic Park', 'Schindler\'s List', 'Saving Private Ryan'],
+  },
+  {
+    name: 'Christopher Nolan',
+    movies: ['Memento', 'The Dark Knight', 'Inception', 'Interstellar', 'Dunkirk'],
+  },
+  {
+    name: 'Martin Scorsese',
+    movies: ['Taxi Driver', 'Raging Bull', 'Goodfellas', 'The Departed', 'The Irishman'],
+  },
+  {
+    name: 'Quentin Tarantino',
+    movies: ['Reservoir Dogs', 'Pulp Fiction', 'Kill Bill: Vol. 1', 'Inglourious Basterds', 'Django Unchained'],
+  },
+  {
+    name: 'Alfred Hitchcock',
+    movies: ['Psycho', 'Vertigo', 'Rear Window', 'North by Northwest', 'The Birds'],
+  },
+  {
+    name: 'James Cameron',
+    movies: ['The Terminator', 'Aliens', 'Titanic', 'Avatar', 'Terminator 2: Judgment Day'],
+  },
+  {
+    name: 'Stanley Kubrick',
+    movies: ['2001: A Space Odyssey', 'A Clockwork Orange', 'The Shining', 'Full Metal Jacket', 'Dr. Strangelove'],
+  },
+  {
+    name: 'Ridley Scott',
+    movies: ['Alien', 'Blade Runner', 'Gladiator', 'The Martian', 'Black Hawk Down'],
+  },
+  {
+    name: 'Peter Jackson',
+    movies: ['The Lord of the Rings: The Fellowship of the Ring', 'The Lord of the Rings: The Two Towers', 'The Lord of the Rings: The Return of the King', 'King Kong', 'The Hobbit: An Unexpected Journey'],
+  },
+  {
+    name: 'Francis Ford Coppola',
+    movies: ['The Godfather', 'The Godfather Part II', 'Apocalypse Now', 'The Conversation', 'The Outsiders'],
+  },
+];
+
 
 function Directors() {
   return (
-    <>
-      <header>
-        {/* What component should go here? */}
-      </header>
-      <main>
-        {/* Director info here! */}
-      </main>
-    </>
+    <div>
+      <NavBar />
+      <h1>Directors Page</h1>
+      {directors.map((director, index) => (
+        <article key={index}>
+          <h2>{director.name}</h2>
+          <ul>
+            {director.movies.map((movie, idx) => (
+              <li key={idx}>{movie}</li>
+            ))}
+          </ul>
+        </article>
+      ))}
+    </div>
   );
-};
+}
 
 export default Directors;
